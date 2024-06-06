0872667775c06f6107dc5d7758c1d5842b02af09
boshra nofal <boshranofal66@gmail.com>
2024-06-06 11:07:16 +0300

    add details page

---
+++ lib/views/pages/product_details_page.dart
@@ -0,0 +1,170 @@
+//import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:food_delivery/models/product_model.dart';
+import 'package:food_delivery/utils/app_colors.dart';
+
+class ProductDetailsPage extends StatelessWidget {
+  final ProductModel product;
+  const ProductDetailsPage({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.favorite_border))
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.network(
+                    product.imgUrl,
+                    height: 200,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    children: [
+                      Text(product.name,
+                          style: const TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold)),
+                      const SizedBox(
+                        width: 100,
+                      ),
+                      Text(
+                        "\$ ${product.price}",
+                        style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: AppColors.primary),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const Row(children: [
+                    Icon(
+                      Icons.star,
+                      color: AppColors.yallow,
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      '5.0(3.5k)',
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 126, 122, 122)),
+                    )
+                  ]),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    product.description,
+                    style: const TextStyle(
+                        color: Color.fromARGB(255, 97, 94, 94), fontSize: 12),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  const Text(
+                    "Add Extra Ingredients",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Center(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 224, 205, 179),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: const Row(
+                        children: [
+                          SizedBox(
+                            width: 16,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Add Cheese",
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 227, 95, 95)),
+                              ),
+                              Text(
+                                "20g  ${'\$2.0'} ",
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    color: Color.fromARGB(255, 92, 86, 86)),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Center(
+                      child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 224, 205, 179),
+                              borderRadius: BorderRadius.circular(20)),
+                          child: const Row(
+                            children: [
+                              SizedBox(
+                                width: 16,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Add Extra Sauce",
+                                    style: TextStyle(
+                                        color:
+                                            Color.fromARGB(255, 227, 95, 95)),
+                                  ),
+                                  Text(
+                                    "50g  ${'\$15.75'} ",
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: Color.fromARGB(255, 92, 86, 86)),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ))),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Center(
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 235, 57, 116),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 32, vertical: 16),
+                          textStyle: TextStyle(fontSize: 20),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          "Add Cart",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  )
+                ],
+              )),
+        ));
+  }
+}
